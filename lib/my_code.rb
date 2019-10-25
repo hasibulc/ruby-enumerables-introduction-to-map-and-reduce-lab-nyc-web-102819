ea213ea2c1f874b7aa1e7a40ada01445e6423fec
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-25 20:03:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,5 +4,11 @@
 
 def map_to_negativize{source_array)
   count = source_array.count
+  negativize = []
+  
   for count in source_array do
-    
+    negativize << source_array[count] * (-1)
+  end
+  
+  return negativize
+end
