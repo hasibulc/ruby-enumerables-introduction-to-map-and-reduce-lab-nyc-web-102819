feaac68600a76c49e4c3d2f087210328e3009248
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-25 20:48:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,7 +54,7 @@ def reduce_to_total(source_array, starting_point = 0)
   total = 0
   
   if starting_point > 0
-    count = starting_point
+    count = starting_point + 1
     while count < source_array.count do
       total += source_array[count]
       count += 1
